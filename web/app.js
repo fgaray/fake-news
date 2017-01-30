2e2f4f42ed584eb58e28e9816a231455e39d0e60
patricio <patricio.jarah@usach.cl>
2017-01-30 18:22:49 -0300

    ADD: carrusel para las fotos y el OCR de la noticia

--- web/app.js
+++ web/app.js
@@ -24,13 +24,13 @@ var uristring = 'mongodb://web_mongo_1/fake-news';
 var theport = process.env.PORT || 5000;
 
 
-mongoose.connect(uristring, function (err, res) {
-  if (err) {
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-    console.log ('Succeeded connected to: ' + uristring);
-  }
-});
+//mongoose.connect(uristring, function (err, res) {
+  //if (err) {
+    //console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  //} else {
+    //console.log ('Succeeded connected to: ' + uristring);
+  //}
+//});
 
 // end database config
 
